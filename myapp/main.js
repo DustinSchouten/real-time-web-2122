798a729ac9027e4380eac363ad3354a9326420e2
DustinSchouten <dustinschouten2@gmail.com>
2022-04-19 11:24:34 +0200

    test

--- myapp/main.js
+++ myapp/main.js
@@ -1,18 +1,25 @@
 require("dotenv").config();
-
 const express = require("express");
 
-var app = express()
+const app = express()
+const http = require('http').createServer(app)
+const path = require('path')
+const io = require('socket.io')(http)
+
 app.set('view engine', 'ejs');
 
-app.use(express.static('public'))
+app.use(express.static(path.resolve('public')))
 
 app.get('/', function (req, res) {
     res.render('index')
 })
 
+io.on('connection', (socket) => {
+    console.log('a user connected');
+});
+
 const port = process.env.PORT || 8000;
 
-app.listen(port, function(){
+http.listen(port, function(){
     console.log("Server listening on port", port);
 })
